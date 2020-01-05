d3076448b670d1a1e5da519762723a7b47186468
Allan Barbosa <allanbn99@gmail.com>
2020-01-05 09:43:52 -0400

    crud do cadastro de usuarios

--- index.php
+++ index.php
@@ -25,7 +25,11 @@ $app->get('/admin', function() {
 
 	User::verifyLogin();
 
-	$page = new PageAdmin();
+	$page = new PageAdmin([
+		"data"=>[
+			"user"=>$_SESSION["User"]["deslogin"]
+		]
+	]);
 
 	$page->setTpl("index");
 
@@ -56,6 +60,109 @@ $app->get('/admin/logout', function() {
 	User::logout();
 
 	header("Location: /admin/login");
+	exit;
+
+});
+
+$app->get('/admin/users', function() {
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin([
+		"data"=>[
+			"user"=>$_SESSION["User"]["deslogin"]
+		]
+	]);
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+
+});
+
+$app->get('/admin/users/create', function() {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin([
+		"data"=>[
+			"user"=>$_SESSION["User"]["deslogin"]
+		]
+	]);
+
+	$page->setTpl("users-create");
+
+});
+
+$app->get('/admin/users/:iduser/delete', function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->get('/admin/users/:iduser', function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin([
+		"data"=>[
+			"user"=>$_SESSION["User"]["deslogin"]
+		]
+	]);
+
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));
+
+});
+
+$app->post('/admin/users/create', function() {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+$app->post('/admin/users/:iduser', function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
 	exit;
 
 });
--- vendor/project/php-classes/src/Model/User.php
+++ vendor/project/php-classes/src/Model/User.php
@@ -60,11 +60,83 @@ class User extends Model {
 
     }
 
-
     public function logout() {
 
         $_SESSION[User::SESSION] = NULL;
 
     }
+
+    public static function listAll() {
+
+        $sql = new Sql();
+
+        return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+    }
+
+    public function save() {
+
+        $sql = new Sql();
+        
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":desperson"=>utf8_decode($this->getdesperson()),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>User::getPasswordHash($this->getdespassword()),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+        ));
+        
+        $this->setData($results[0]);
+        
+    }
+    
+    public function get($iduser) {
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+            ":iduser"=>$iduser
+        ));
+
+        $this->setData($results[0]);
+
+    }
+
+    public function update() {
+
+        $sql = new Sql();
+        
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            ":iduser"=>$this->getiduser(),
+            ":desperson"=>utf8_decode($this->getdesperson()),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>User::getPasswordHash($this->getdespassword()),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+        ));
+        
+        $this->setData($results[0]);
+
+    }
+
+    public function delete() {
+
+        $sql = new Sql();
+
+        $sql->query("CALL sp_users_delete(:iduser)", array(
+            ":iduser"=>$this->getiduser()
+        ));
+
+    }
+
+    public static function getPasswordHash($password) {
+
+		return password_hash($password, PASSWORD_DEFAULT, [
+			'cost'=>12
+        ]);
+        
+	}
 
 }
