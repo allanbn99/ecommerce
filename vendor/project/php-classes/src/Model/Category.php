ef301c7ad3ac5a23b93c8a0b46b4d7c00a892981
Allan Barbosa <allanbn99@gmail.com>
2020-01-20 21:30:35 -0400

    Produtos vs Categorias

--- vendor/project/php-classes/src/Model/Category.php
+++ vendor/project/php-classes/src/Model/Category.php
@@ -4,6 +4,7 @@ namespace Project\Model;
 
 use \Project\Db\Sql;
 use \Project\Model;
+use \Project\Model\Product;
 
 class Category extends Model {
 
@@ -84,6 +85,60 @@ class Category extends Model {
         file_put_contents($_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
 
     }
+
+    public function getProducts($related = true) {
+
+        $sql = new Sql();
+
+        if ($related) {
+
+            $results = $sql->select("
+                SELECT * FROM tb_products WHERE idproduct IN(
+                    SELECT a.idproduct FROM tb_products a INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct WHERE b.idcategory = :idcategory
+                )
+            ", array(
+                ":idcategory"=>$this->getidcategory()
+            ));
+
+            return $results;
+
+        } else {
+
+            $results = $sql->select("
+                SELECT * FROM tb_products WHERE idproduct NOT IN(
+                    SELECT a.idproduct FROM tb_products a INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct WHERE b.idcategory = :idcategory
+                )
+            ", array(
+                ":idcategory"=>$this->getidcategory()
+            ));
+
+            return $results;
+
+        }
+
+    }
+
+    public function addProduct(Product $product) {
+
+        $sql = new Sql();
+
+        $sql->query("INSERT INTO tb_productscategories (idcategory, idproduct) VALUES (:idcategory, :idproduct)", array(
+            ':idcategory'=>$this->getidcategory(),
+            ':idproduct'=>$product->getidproduct()
+        ));
+
+    }
+
+    public function removeProduct(Product $product) {
+
+        $sql = new Sql();
+
+        $sql->query("DELETE FROM tb_productscategories WHERE idcategory = :idcategory AND idproduct = :idproduct", array(
+            ':idcategory'=>$this->getidcategory(),
+            ':idproduct'=>$product->getidproduct()
+        ));
+
+    } 
 
 }
 
